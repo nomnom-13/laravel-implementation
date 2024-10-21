05db4bb4bb43af8fce564dfdd11c959a16d2254c
nomnom-13 <santosraymond437@gmail.com>
2024-10-21 16:50:56 +0800

    transfer html, css, js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 import './bootstrap';
+
+const links = document.querySelectorAll("a");
+
+window.addEventListener('scroll', () => {
+const scrollPosition = window.scrollY;
+const viewportHeight = window.innerHeight;
+
+links.forEach(link => {
+    const sectionId = link.getAttribute('href');
+    const targetSection = document.querySelector(sectionId);
+    
+    if (targetSection) {
+        const sectionTop = targetSection.offsetTop;
+        const sectionBottom = sectionTop + targetSection.offsetHeight;
+
+        if (scrollPosition >= sectionTop - viewportHeight / 2 && scrollPosition < sectionBottom - viewportHeight / 2) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    }
+});
+});
